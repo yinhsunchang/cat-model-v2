299a20e7535f36a30dc7b5a5b2468b707f4577c8
yinhsunchang <yinhsunchang@gmail.com>
2026-07-27 02:54:08 +0200

    feat: add ReservationForm component

---
+++ src/components/ReservationForm.tsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { sendReservationForm } from "../services/reservation";
+import type { FormProps } from "../types/reservation";
+import { useTranslation } from "react-i18next";
+
+interface SuccessProps {
+  onSuccess: () => void;
+}
+
+const ReservationForm = ({ onSuccess }: SuccessProps) => {
+  const { t } = useTranslation();
+  const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [form, setForm] = useState<FormProps>({
+    name: "",
+    email: "",
+    date: "",
+    message: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setForm((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    setError(null);
+
+    try {
+      await sendReservationForm(form);
+      setSuccess(t("about.reserve.success"));
+      setForm({ name: "", email: "", date: "", message: "" });
+      setTimeout(onSuccess, 1500);
+    } catch (err) {
+      console.error(err);
+      setError(t("about.reserve.error"));
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <p>
+          <input
+            className="input padding-16 border"
+            type="text"
+            name="name"
+            value={form.name}
+            placeholder={t("about.reserve.name")}
+            onChange={handleChange}
+            disabled={loading}
+            required
+          />
+        </p>
+
+        <p>
+          <input
+            className="input padding-16 border"
+            type="email"
+            name="email"
+            value={form.email}
+            placeholder="Email"
+            onChange={handleChange}
+            disabled={loading}
+            required
+          />
+        </p>
+
+        <p>
+          <input
+            className="input padding-16 border"
+            type="datetime-local"
+            name="date"
+            value={form.date}
+            onChange={handleChange}
+            disabled={loading}
+            required
+          />
+        </p>
+
+        <p>
+          <input
+            className="input padding-16 border"
+            type="text"
+            name="message"
+            value={form.message}
+            placeholder="Message"
+            onChange={handleChange}
+            disabled={loading}
+            required
+          />
+        </p>
+
+        {success && <p style={{ color: "green" }}>{success}</p>}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+
+        <p>
+          <button
+            className="button black padding-16 margin-bottom"
+            type="submit"
+            disabled={loading}
+          >
+            <i className="fa fa-paper-plane"></i> {t("about.reserve.send")}
+            {loading ? "Sending..." : null}
+          </button>
+        </p>
+      </form>
+    </>
+  );
+};
+
+export default ReservationForm;
