0ebe131fa1fb55ca505ea02fe1e0037eb2ac7646
yinhsunchang <yinhsunchang@gmail.com>
2026-07-27 02:18:17 +0200

    refactor(types): rename form.ts to contact.ts

--- src/services/contact.ts
+++ src/services/contact.ts
@@ -1,5 +1,5 @@
 import { supabase } from "../lib/supabase";
-import type { FormProps } from "../types/form";
+import type { FormProps } from "../types/contact";
 
 export async function sendContactForm(form: FormProps): Promise<void> {
   const { error } = await supabase.from("contacts").insert(form);
