bcb6eeea62adc16717a85239dbb6161753586826
yinhsunchang <yinhsunchang@gmail.com>
2026-07-28 19:35:55 +0200

    refactor: add id field to photo objects

--- src/services/photos.ts
+++ src/services/photos.ts
@@ -10,23 +10,42 @@ import tunnel from "../assets/tunnel.jpg";
 import closeup from "../assets/closeup.jpg";
 import waiting from "../assets/waiting.jpg";
 
-interface Photo {
-  src: string;
-  altKey: string;
-}
+import type { Photo } from "../types/photo";
 
 const photos: Photo[] = [
-  { src: reading, altKey: "photo.reading" },
-  { src: walking, altKey: "photo.walking" },
-  { src: tongue, altKey: "photo.tongue" },
-  { src: sink2, altKey: "photo.sink2" },
-  { src: sink1, altKey: "photo.sink1" },
-  { src: paw, altKey: "photo.paw" },
-  { src: curious, altKey: "photo.curious" },
-  { src: faucet, altKey: "photo.faucet" },
-  { src: tunnel, altKey: "photo.tunnel" },
-  { src: closeup, altKey: "photo.closeup" },
-  { src: waiting, altKey: "photo.waiting" },
+  { id: 101, src: reading, alt: "photo.reading" },
+  { id: 102, src: walking, alt: "photo.walking" },
+  { id: 103, src: tongue, alt: "photo.tongue" },
+  { id: 104, src: sink2, alt: "photo.sink2" },
+  { id: 105, src: sink1, alt: "photo.sink1" },
+  { id: 106, src: paw, alt: "photo.paw" },
+  { id: 107, src: curious, alt: "photo.curious" },
+  { id: 108, src: faucet, alt: "photo.faucet" },
+  { id: 109, src: tunnel, alt: "photo.tunnel" },
+  { id: 110, src: closeup, alt: "photo.closeup" },
+  { id: 111, src: waiting, alt: "photo.waiting" },
+  { id: 112, src: reading, alt: "photo.reading" },
+  { id: 113, src: walking, alt: "photo.walking" },
+  { id: 114, src: tongue, alt: "photo.tongue" },
+  { id: 115, src: sink2, alt: "photo.sink2" },
+  { id: 116, src: sink1, alt: "photo.sink1" },
+  { id: 117, src: paw, alt: "photo.paw" },
+  { id: 118, src: curious, alt: "photo.curious" },
+  { id: 119, src: faucet, alt: "photo.faucet" },
+  { id: 120, src: tunnel, alt: "photo.tunnel" },
+  { id: 121, src: closeup, alt: "photo.closeup" },
+  { id: 122, src: waiting, alt: "photo.waiting" },
+  { id: 123, src: reading, alt: "photo.reading" },
+  { id: 124, src: walking, alt: "photo.walking" },
+  { id: 125, src: tongue, alt: "photo.tongue" },
+  { id: 126, src: sink2, alt: "photo.sink2" },
+  { id: 127, src: sink1, alt: "photo.sink1" },
+  { id: 128, src: paw, alt: "photo.paw" },
+  { id: 129, src: curious, alt: "photo.curious" },
+  { id: 130, src: faucet, alt: "photo.faucet" },
+  { id: 131, src: tunnel, alt: "photo.tunnel" },
+  { id: 132, src: closeup, alt: "photo.closeup" },
+  { id: 133, src: waiting, alt: "photo.waiting" },
 ];
 
 export default photos;
