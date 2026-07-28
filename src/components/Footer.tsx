ab4bb47aff960a2110223ffe063aab053d515e63
yinhsunchang <yinhsunchang@gmail.com>
2026-07-28 21:36:29 +0200

    style: format code with Prettier

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,22 @@
 const Footer = () => {
   return (
-  <footer className="content padding-top-64 text-grey xxlarge">
-  <a href="https://www.instagram.com/docteur.2022/" target="_blank">
-    <i className="fa fa-instagram hover-opacity"></i>
-  </a>
-  <p className="medium">Designed & Built by 
-    <a href="https://github.com/yinhsunchang" target="_blank" className="hover-text-white"> Yin-Hsun Chang</a>
-  </p>
-  </footer>
+    <footer className="content padding-top-64 text-grey xxlarge">
+      <a href="https://www.instagram.com/docteur.2022/" target="_blank">
+        <i className="fa fa-instagram hover-opacity"></i>
+      </a>
+      <p className="medium">
+        Designed & Built by
+        <a
+          href="https://github.com/yinhsunchang"
+          target="_blank"
+          className="hover-text-white"
+        >
+          {" "}
+          Yin-Hsun Chang
+        </a>
+      </p>
+    </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
