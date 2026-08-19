150cfc4d262be6f830441df634608a313eb5ce55
yinhsunchang <yinhsunchang@gmail.com>
2026-08-19 21:02:50 +0200

    feat: add Encyclopedia component

---
+++ src/pages/box/Encyclopedia.tsx
@@ -0,0 +1,13 @@
+import CatFact from "./CatFact.tsx";
+
+const Encyclopedia = () => {
+  return (
+    <>
+      <div className="padding-large">
+        <CatFact />
+      </div>
+    </>
+  );
+};
+
+export default Encyclopedia;
