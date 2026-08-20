94d6bf54142c79a879fb91921adddc0e75e3a871
yinhsunchang <yinhsunchang@gmail.com>
2026-08-20 21:51:34 +0200

    style: align about section icons with text

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -11,12 +11,18 @@ const About = () => {
     <div className="content justify text-light-grey padding-64" id="about">
       <h2>{t("about.title")}</h2>
       <hr style={{ width: "200px" }} className="opacity" />
-      <i className="fa fa-certificate xxlarge"></i>
-      <p>{t("about.texts.race")}</p>
-      <i className="fa fa-birthday-cake xxlarge"></i>
-      <p>{t("about.texts.birthday")}</p>
-      <i className="fa fa-home xxlarge"></i>
-      <p>{t("about.texts.country")}</p>
+      <p className="large">
+        <i className="fa fa-certificate margin-right"></i>
+        {t("about.texts.race")}
+      </p>
+      <p className="large">
+        <i className="fa fa-birthday-cake margin-right"></i>
+        {t("about.texts.birthday")}
+      </p>
+      <p className="large">
+        <i className="fa fa-home margin-right"></i>
+        {t("about.texts.country")}
+      </p>
 
       <h3 className="padding-16 text-light-grey">{t("about.skills.title")}</h3>
       <SkillBar label={t("about.skills.sleep")} percentage={95} />
