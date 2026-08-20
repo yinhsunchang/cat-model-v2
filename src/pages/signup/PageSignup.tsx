92754458090c2d58b0c12e1202b9ddb1a76c6eea
yinhsunchang <yinhsunchang@gmail.com>
2026-08-20 22:17:42 +0200

    feat: add PageSignup component

---
+++ src/pages/signup/PageSignup.tsx
@@ -0,0 +1,13 @@
+import SignupSupa from "./SignupSupa.tsx";
+
+const PageLogin = () => {
+  return (
+    <>
+      <div className="padding-large">
+        <SignupSupa />
+      </div>
+    </>
+  );
+};
+
+export default PageLogin;
